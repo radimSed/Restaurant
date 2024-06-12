39290d9b7672a87472dd490536786c0b88031d22
Radim Sedláček <radim.sed@email.cz>
2024-06-12 17:24:34 +0200

    Added import from file

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class Recipe {
+import java.io.*;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Recipe {
+    private  String title;
+    private BigDecimal price;
+    private int preparationTime;
+    private String pictureName;
+    GlobalVariables globalVars = new GlobalVariables();
+
+    public Recipe(String title, BigDecimal price, int preparationTime, String pictureName) throws RestaurantException{
+        this.title = title;
+        this.price = price;
+        setPreparationTime(preparationTime);
+        this.pictureName = pictureName;
+    }
+
+    public Recipe(String title, BigDecimal price, int preparationTime) throws RestaurantException{
+        this.title = title;
+        this.price = price;
+        setPreparationTime(preparationTime);
+        this.pictureName = "blank";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(int preparationTime) throws RestaurantException{
+        if (preparationTime < 0){
+            throw new RestaurantException("Preparation time must be larger than 0. Recipe was not created.");
+        } else {
+            this.preparationTime = preparationTime;
+        }
+    }
+
+    public String getPictureName() {
+        return pictureName;
+    }
+
+    public void setPictureName(String pictureName) {
+        this.pictureName = pictureName;
+    }
+
+    public void exportToFile() throws RestaurantException{
+        //filename = title of the meal + .txt
+        String delimiter = GlobalVariables.getDelimiter();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(this.title + ".txt"))){
+            bw.write(this.title + delimiter + this.price + delimiter + this.preparationTime + delimiter + this.pictureName);
+        } catch (IOException e){
+            throw new RestaurantException(e.getMessage());
+        }
+    }
+
+    public static Recipe importFromFile(String path) throws RestaurantException{
+        String delimiter = GlobalVariables.getDelimiter();
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            Scanner scanner = new Scanner(br);
+            String line = scanner.nextLine();
+            String parts[] = line.split(delimiter);
+            return new Recipe(parts[0], new BigDecimal(parts[1]) , Integer.parseInt(parts[2]), parts[3]);
+        } catch (IOException e){
+            throw new RestaurantException(e.getMessage());
+        }
+    }
 }
