5ef119f31a6df0b466691af6b2df77ad9d8c2fed
Radim Sedláček <radim.sed@email.cz>
2024-06-13 13:07:09 +0200

    Added getter/setter methods

--- src/Order.java
+++ src/Order.java
@@ -2,7 +2,7 @@ import java.time.LocalTime;
 
 public class Order {
     private static int id = 0;
-    private int orderId;
+    private int orderId;  //should be unigue in whole system
     private int tableNumber;
     private int mealId;
     private int amount;
@@ -19,5 +19,61 @@ public class Order {
         this.status = OrderStatus.ORDERED;
         this.orderedTime = LocalTime.now();
 
+    }
+
+    public int getId(){
+        return this.orderId;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public int getMealId() {
+        return mealId;
+    }
+
+    public void setMealId(int mealId) {
+        this.mealId = mealId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public LocalTime getOrderedTime() {
+        return orderedTime;
+    }
+
+    public void setOrderedTime(LocalTime orderedTime) {
+        this.orderedTime = orderedTime;
+    }
+
+    public LocalTime getFulfilmentTime() {
+        return fulfilmentTime;
+    }
+
+    public void setFulfilmentTime(LocalTime fulfilmentTime) {
+        this.fulfilmentTime = fulfilmentTime;
+    }
+
+    public int getOrderId(){
+        return this.orderId;
     }
 }
